90731b7c87f6f1d3e79ba63cd620bc488871ce9c
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-27 18:45:02 +0900

    캐릭터 출력 및 esc 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,1 +1,35 @@
+from pico2d import*
 
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH,TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+
+def handle_events():
+    global running, dir
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+
+
+
+while running:
+    clear_canvas()
+
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(0,900,125,125,300,400)
+
+    update_canvas()
+
+    handle_events()
+
+    if not running:
+        break
