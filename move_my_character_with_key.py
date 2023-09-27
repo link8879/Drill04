d9344ffaf7d961b5a2ea621529546c80941452a7
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-27 23:25:10 +0900

    오른쪽 이동 완성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,28 +8,55 @@ character = load_image('animation_sheet.png')
 running = True
 
 def handle_events():
-    global running, dir
+    global running, dir_x, dir_y
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.type == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
 
 
+frame = 0
+x = 400
+y = 400
+dir_x = 0
+dir_y = 0
 
+def draw():
+    global frame, x, y
 
+    if dir_x == 1 and dir_y == 0:
+        character.clip_draw(frame * 80, 0, 80, 80, x, y)
+        frame = (frame + 1) % 10
+        x += dir_x * 10
+    
+    elif dir_x == 0 and dir_y == 0:
+        character.clip_draw(frame * 80, 0, 80, 80, x, y)
+        frame = (frame + 1) % 3
 
 while running:
     clear_canvas()
 
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(0,900,125,125,300,400)
+
+    draw()
 
     update_canvas()
-
     handle_events()
 
     if not running:
         break
+
+    delay(0.15)
